d48bcb3a2d2b77f8e103acec4ed5c8420b877872
nova_nghia <nghia922841@gmail.com>
2024-11-06 22:10:54 +0700

    doc: add swagger doc for order API

--- src/main/java/com/example/regionaldelicacy/controller/OrderController.java
+++ src/main/java/com/example/regionaldelicacy/controller/OrderController.java
@@ -13,6 +13,11 @@ import com.example.regionaldelicacy.dto.OrderInfoDto;
 import com.example.regionaldelicacy.dto.OrderRequestDto;
 import com.example.regionaldelicacy.services.OrderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -21,16 +26,26 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
+@Tag(name = "Order", description = "API for making and viewing orders")
 public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Creating a new order from selected user's cart item", 
+    description = "Getting a list of ID of the cart items that user selected and the information of the customer to deliver, creating a new order for that user.")
+    @ApiResponse(responseCode = "200", description = "Order has been created succesfully", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = OrderInfoDto.class))
+    })
     @PostMapping
     public ResponseEntity<OrderInfoDto> createOrder(@RequestBody @Valid OrderRequestDto orderRequest) {
         OrderInfoDto createdOrder = orderService.createOrder(orderRequest);
         return ResponseEntity.ok(createdOrder);
     }
 
+    @Operation(summary = "Get all orders of current user", description = "Return a list contains info of each orders of current user")
+    @ApiResponse(responseCode = "200", description = "Order list has been retrived succesfully", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = OrderInfoDto[].class))
+    })
     @GetMapping
     public ResponseEntity<List<OrderInfoDto>> getOrdersForCurrentUser(
         @RequestParam(defaultValue = "0") Integer page,
@@ -40,6 +55,11 @@ public class OrderController {
         return ResponseEntity.ok(orders);
     }
 
+    @Operation(summary = "Get info of an order by orderId", 
+    description = "Return the info of the order that matches the provided orderId")
+    @ApiResponse(responseCode = "200", description = "Order has been created succesfully", content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = OrderInfoDto.class))
+    })
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderInfoDto> getOrderByOrderIdForCurrentUser(@PathVariable Long orderId) {
         OrderInfoDto order = orderService.getOrderByUserIdAndOrderId(orderId);
