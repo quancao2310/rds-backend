dbf76b71a0223bda09deb065075a2e2d419b762a
Quan Cao <quancao2310@gmail.com>
2024-12-20 20:51:22 +0700

    fix: fix validation for cart dtos

--- src/main/java/com/example/regionaldelicacy/dtos/CartUpdateDto.java
+++ src/main/java/com/example/regionaldelicacy/dtos/CartUpdateDto.java
@@ -1,12 +1,12 @@
 package com.example.regionaldelicacy.dtos;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class CartUpdateDto {
-    @NotBlank(message = "Quantity is required")
+    @NotNull(message = "Quantity is required")
     @Positive(message = "Quantity must be positive")
     private Integer quantity;
 }
